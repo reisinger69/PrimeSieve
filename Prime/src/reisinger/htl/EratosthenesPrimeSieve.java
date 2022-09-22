0f9210ed2a85cbdf500f5192e497198152ce3de0
reisinger69 <basti4772@gmail.com>
2022-09-22 18:59:47 +0200

    constructor inplementiert

--- Prime/src/reisinger/htl/EratosthenesPrimeSieve.java
+++ Prime/src/reisinger/htl/EratosthenesPrimeSieve.java
@@ -4,13 +4,22 @@ import java.util.ArrayList;
 
 public class EratosthenesPrimeSieve implements PrimeSieve {
 
+    public EratosthenesPrimeSieve(int max) {
+        boolean[] isPrimes = new boolean[max];
+        for (int i = 0; i < max; i++) {
+            isPrimes[i] = isPrime(i);
+        }
+    }
+
     @Override
     public boolean isPrime(int p) {
         int [] temp = new int [p+1];
         temp [0] = 2;
+
         int index = 1;
         int prime = 1;
         boolean isPrime = false;
+
         while((prime += 2) <= p+1) {
             isPrime = true;
             for(int i = 0; i < index; i++) {
--- Prime/src/reisinger/htl/Main.java
+++ Prime/src/reisinger/htl/Main.java
@@ -3,7 +3,6 @@ package reisinger.htl;
 public class Main {
 
     public static void main(String[] args) {
-	    EratosthenesPrimeSieve prime = new EratosthenesPrimeSieve();
-        System.out.println(prime.isPrime(11));
+
     }
 }
